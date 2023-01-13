c7a1b02caa6b6ab0264a2fa2e1d1ec0fea52e2ce
George Amuzu <gnamuzu@gmail.com>
2023-01-13 19:46:51 +0000

    migration to handle changes in user_details table

--- migrations/sqls/20230113193542-user-details-up.sql
+++ migrations/sqls/20230113193542-user-details-up.sql
@@ -1,1 +1,13 @@
 /* Replace with your SQL commands */
+
+-- Table: user_details
+
+CREATE TABLE user_details
+(
+    sn SERIAL PRIMARY KEY,
+    user_id VARCHAR(20),
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    username VARCHAR(50),
+    password VARCHAR(100)
+);
