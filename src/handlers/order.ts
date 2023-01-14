82efd07e0cb2ba123719aadcb598c9d3155b7ff2
George Amuzu <gnamuzu@gmail.com>
2023-01-14 13:57:53 +0000

    added remove function to order handler

--- src/handlers/order.ts
+++ src/handlers/order.ts
@@ -20,7 +20,7 @@ const create = async (req: Request, res: Response) => {
 };
 
 const read = async (req: Request, res: Response) => {
-    const order_id: number = +req.params.sn;
+    const order_id: number = +req.params.id;
     try {
         const order = await store.read(order_id);
         res.status(200).json({
@@ -32,3 +32,15 @@ const read = async (req: Request, res: Response) => {
     }
 };
 
+const remove = async (req: Request, res: Response) => {
+    const order_id: number = +req.params.id;
+    try{
+        const remove_order = await store.delete(order_id);
+        res.status(200).json({
+            results: remove_order,
+            message: "Order with ID: " + order_id + " was deleted successfully."
+        });
+    }catch (error)  {
+        res.status(400).send({message: "Failed to delete order because, " + error});
+    }
+};
