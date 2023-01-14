f59a8de0ab3b8a8dc4ffb8f54427161e364748d0
George Amuzu <gnamuzu@gmail.com>
2023-01-14 13:46:48 +0000

    added create function to order handler

--- src/handlers/order.ts
+++ src/handlers/order.ts
@@ -3,3 +3,19 @@ import { Order, OrderStore } from "../models/order";
 
 const store = new OrderStore();
 
+const create = async (req: Request, res: Response) => {
+    const order: Order = {
+        user_id: req.body.user_id,
+        status: req.body.status,
+    }
+    try {
+        const newOrder = await store.create(order);
+        res.status(200).json({
+            results: newOrder,
+            message: "New order with ID: " + newOrder.sn + " was added successfully"
+        });
+    }catch(error) {
+        res.status(400).send({message: "Failed to create order because, " + error});
+    }
+};
+
