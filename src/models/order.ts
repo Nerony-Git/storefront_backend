145a325a3adc6af78554bec2d19d6a7d6990e92b
George Amuzu <gnamuzu@gmail.com>
2023-01-14 10:16:34 +0000

    created create function for order model

--- src/models/order.ts
+++ src/models/order.ts
@@ -8,5 +8,19 @@ export type Order = {
 };
 
 export class OrderStore {
+    async create(o: Order): Promise<Order> {
+        try {
+            const conn = await client.connect();
+            const sql = "INSERT INTO orders (user_id, status) VALUES ($1, $2) RETURNING *";
+            const result = await conn.query(sql, [o.user_id, o.status]);
+            const order = result.rows[0];
+            conn.release();
+
+            return order;
+        }catch(error){
+            throw new Error("Unable to create order with ID:" + o.user_id + " because, " + error);
+        }
+    }
+
     
 }
