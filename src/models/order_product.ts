2611b943b2a0e67e92ad87c30008f53b67312525
George Amuzu <gnamuzu@gmail.com>
2023-01-14 10:54:59 +0000

    created addproduct function for orderproduct model

--- src/models/order_product.ts
+++ src/models/order_product.ts
@@ -8,6 +8,19 @@ export type Order_Product = {
 }
 
 export class Order_ProductStore{
-    
+    async addProduct(op: Order_Product): Promise<Order_Product> {
+        try {
+            const conn = await client.connect();
+            const sql = "INSERT INTO order_products (quantity, order_id, product_id) VALUES ($1, $2, $3) RETURNING *";
+            const result = await conn.query(sql, [op.quantity, op.order_id, op.product_id]);
+            const order = result.rows[0];
+            conn.release();
+
+            return order;
+        }catch (error){
+            throw new Error("Product with ID: " + op.product_id + " can't be added to order with ID: " + op.order_id + " because, " + error);
+            
+        }
+    }
 
 }
