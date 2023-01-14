d40bac5696e9e587d90d1223f3257757fe165313
George Amuzu <gnamuzu@gmail.com>
2023-01-14 10:31:03 +0000

    created read function for order model

--- src/models/order.ts
+++ src/models/order.ts
@@ -22,5 +22,18 @@ export class OrderStore {
         }
     }
 
-    
+    async read(oid: number): Promise<Order> {
+        try{
+            const conn = await client.connect();
+            const sql = "SELECT orders.id, orders.user_id, orders.status, order_products.product_id, order_products.quantity FROM 'orders' INNER JOIN 'order_products' ON orders.sn = order_products.order_id WHERE orders.sn = $1";
+            const result = await conn.query(sql,[oid]);
+            const order = result.rows[0];
+            conn.release();
+
+            return order;
+        }catch (error){
+            throw new Error("Failed to load order details of order with ID: " + oid + " because, " + error);
+        }
+    }
+
 }
