502cb9336541ced854f2bf4e94c4f8fbefe65bd5
George Amuzu <gnamuzu@gmail.com>
2023-01-14 14:11:07 +0000

    created orders routes

---
+++ src/routes/orders.ts
@@ -0,0 +1,13 @@
+import { Application } from "express";
+import token_authenticator from "../middleware/verify_jwt";
+import * as order_handler from "../handlers/order";
+
+const order_routes = (app: Application) => {
+    app.get("/orders", token_authenticator, order_handler.default.index);
+    app.get("/orders/:id", token_authenticator, order_handler.default.read);
+    app.post("/orders", token_authenticator, order_handler.default.create);
+    app.delete("/orders/:id", token_authenticator, order_handler.default.remove)
+
+};
+
+export default order_routes;
