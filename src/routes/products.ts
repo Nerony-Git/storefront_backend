dd907d5a32614b3d4bdec38722d5436a7d20957f
George Amuzu <gnamuzu@gmail.com>
2023-01-14 13:34:10 +0000

    created products routes

---
+++ src/routes/products.ts
@@ -0,0 +1,14 @@
+import { Application } from "express";
+import token_authenticator from "../middleware/verify_jwt";
+import * as product_detail_handler from "../handlers/product_detail";
+
+const products_routes = (app: Application) => {
+    app.get("/products", product_detail_handler.default.index);
+    app.get("/products/:id", product_detail_handler.default.read);
+    app.post("/products", token_authenticator, product_detail_handler.default.create);
+    app.put("/products/:id", token_authenticator, product_detail_handler.default.update);
+    app.delete("/products/:id", token_authenticator, product_detail_handler.default.remove)
+}
+
+
+export default products_routes;
