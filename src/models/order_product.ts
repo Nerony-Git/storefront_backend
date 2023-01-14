f769df444fa8940f0aad7faf7eea1261487d5c9b
George Amuzu <gnamuzu@gmail.com>
2023-01-14 10:45:34 +0000

    created model for order product table

---
+++ src/models/order_product.ts
@@ -0,0 +1,13 @@
+import client from "../database";
+
+export type Order_Product = {
+    sn?: number;
+    quantity: number;
+    order_id: string;
+    product_id: string;
+}
+
+export class Order_ProductStore{
+    
+
+}
