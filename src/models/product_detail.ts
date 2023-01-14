ff7479dc0078a60118099f0b3c8fe2e26120a458
George Amuzu <gnamuzu@gmail.com>
2023-01-14 09:47:55 +0000

    created create function for product details model

--- src/models/product_detail.ts
+++ src/models/product_detail.ts
@@ -13,7 +13,15 @@ export class ProductStore {
     async create(p: Product): Promise<Product> {
         try {
             const conn = await client.connect();
-            const sql = "INSERT INTO product_details ()";
+            const sql = "INSERT INTO product_details (product_id, product_name, product_price, product_category) VALUES ($1, $2, $3, $4) RETURNING *";
+            const result = await conn.query(sql, [p.product_id, p.product_name, p.product_price, p.product_category]);
+            const product = result.rows[0];
+            conn.release();
+
+            return product;
+        }catch (error){
+            throw new Error("Unable to create product: " + p.product_name + " because, " + error);
         }
     }
+
 }
