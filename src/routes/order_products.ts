b8d80f54678371651165323d8aa45124d2779f14
George Amuzu <gnamuzu@gmail.com>
2023-01-22 16:11:01 +0000

    updated route to include delete function

--- src/routes/order_products.ts
+++ src/routes/order_products.ts
@@ -1,9 +1,10 @@
 import { Application } from "express";
 import * as token_process from "../middleware/verify_jwt";
-import addProduct from "../handlers/order_product";
+import * as order_product_handler from "../handlers/order_product";
 
 const order_product_route = (app: Application) => {
-    app.post("/orders/products/add", token_process.default.token_authenticator, addProduct);
+    app.post("/orders/products/add", token_process.default.token_authenticator, order_product_handler.default.addProduct);
+    app.delete("/orders/products/:id", token_process.default.token_authenticator, order_product_handler.default.remove);
 };
 
 export default order_product_route;
